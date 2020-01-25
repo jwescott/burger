0bd08826659f7c299e44ab5b20fa36437a801c97
jwescott <jwescott@mail.usf.edu>
2020-01-24 22:01:07 -0500

    added the structure of the files

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+USE burgers_db;
+
+INSERT INTO burgers(
+    burger_name, devoured
+)
+
+VALUES (
+    "Big Mac", false
+    ),(
+        "Whopper", false
+    ),(
+        "Baconator", false
+    )
